6f048c249547d17b8b287498f3b5f0f71009c5d9
Euclides Paes de Melo Junior <epmelo.ia@gmail.com>
2025-10-29 20:34:31 -0300

    Descr

--- github/sincronizar-github-pc-para-web-e-web-para-pc/sincronizar-pc-web.sh
+++ github/sincronizar-github-pc-para-web-e-web-para-pc/sincronizar-pc-web.sh
@@ -22,48 +22,66 @@ echo '# ------------------------------ #'
 echo '#  INICIO DO SINCRONISMO PC-WEB  #'
 echo '# ------------------------------ #'
 echo
+
 echo '# -> cd /d/_GITHUB/templates <- #'
 echo
 cd /d/_GITHUB/templates
+
 echo
 echo '# -> pwd <- #'
 echo
 pwd
+
 echo
 echo
 read -p "Pressione ENTER para continuar..."
+echo
+
 clear
+
 echo
 echo
+
 echo '# -> git status <- #'
 echo
 git status
+
+echo
 echo
 read -p "Pressione ENTER para continuar..."
 echo
 echo
+
 echo '# -> git add .'
 echo
 git add .
+
 echo
+echo
+
 echo '# -> git commit -m "Descr."'
 echo
 git commit -m "Descr"
+
 echo
 echo
 read -p "Pressione ENTER para continuar..."
 echo
 echo
+
 echo '# -> git push'
 echo
 git push
+
 echo
 read -p "Pressione ENTER para continuar..."
 echo
 echo
+
 echo '# -> git pull'
 echo
 git pull
+
 echo
 echo
 echo
