d8a5411d45b29ad853ff5caadf2059dde6cd71b0
Euclides Paes de Melo Junior <epmelo.ia@gmail.com>
2025-10-29 20:24:31 -0300

    Descr

--- github/sincronizar-github-pc-para-web-e-web-para-pc/sincronizar-pc-web.sh
+++ github/sincronizar-github-pc-para-web-e-web-para-pc/sincronizar-pc-web.sh
@@ -17,44 +17,60 @@
 ## Versão com Commit Fixo
 ##
 clear
-echo '# INICIO DO SINCRONISMO PC-WEB #'
-echo '# ---------------------------- #'
 echo
-echo '# -> cd /d/_GITHUB/templates <-#'
+echo '# ------------------------------ #'
+echo '#  INICIO DO SINCRONISMO PC-WEB  #'
+echo '# ------------------------------ #'
+echo
+echo '# -> cd /d/_GITHUB/templates <- #'
+echo
+echo
 cd /d/_GITHUB/templates
 echo
-
-echo 'pwd'
+echo
+echo '# -> pwd <- #'
+echo
 pwd
 echo
 read -p "Pressione ENTER para continuar..."
 echo
-
-echo 'git status'
+echo
+echo '# -> git status <- #'
+echo
 git status
 echo
 read -p "Pressione ENTER para continuar..."
 echo
-
-echo 'git add .'
+echo
+echo '# -> git add .'
+echo
 git add .
+echo
+echo
+echo
+echo '# -> git commit -m "Descr."'
+echo
+git commit -m "Descr"
 echo
 read -p "Pressione ENTER para continuar..."
 echo
-
-echo 'git commit -m "Descr."'
-git commit -m "Descr"
 echo
-echo 'git push'
+echo '# -> git push'
+echo
 git push
 echo
 read -p "Pressione ENTER para continuar..."
 echo
-
-echo 'git pull'
+echo
+echo '# -> git pull'
+echo
 git pull
 echo
 echo
-echo 'FIM DO SINCRONISMO PC-WEB'
+echo
+echo '# --------------------------- #'
+echo '#  FIM DO SINCRONISMO PC-WEB  #'
+echo '# --------------------------- #'
+echo
 echo
 echo
