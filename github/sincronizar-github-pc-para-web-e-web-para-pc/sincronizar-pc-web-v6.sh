edd6d9cc3711079aea3ff6623897d37c2e1ded35
Euclides Paes de Melo Junior <epmelo.ia@gmail.com>
2025-11-25 16:53:55 -0300

    Descr

---
+++ github/sincronizar-github-pc-para-web-e-web-para-pc/sincronizar-pc-web-v6.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Gera nome do log com data e hora
+DATA=$(date +"%Y-%m-%d")
+HORA=$(date +"%Hh%Mm%Ss")
+LOG="D:/_GITHUB/logs/LOG-PC-WEB-$DATA-$HORA.txt"
+
+clear
+
+{
+echo
+echo '# ------------------------------ #'
+echo '#  INICIO DO SINCRONISMO PC-WEB  #'
+echo '# ------------------------------ #'
+echo
+
+echo '# -> cd /d/_GITHUB/templates <- #'
+cd /d/_GITHUB/templates
+
+echo '# -> pwd <- #'
+pwd
+
+echo .
+
+echo '# -> git status <- #'
+git status
+
+echo '# -> git add .'
+git add .
+
+echo '# -> git commit -m "Descr."'
+git commit -m "Descr"
+
+echo '# -> git push'
+git push
+
+echo '# -> git pull'
+git pull
+
+echo '# --------------------------- #'
+echo '#  FIM DO SINCRONISMO PC-WEB  #'
+echo '# --------------------------- #'
+} | tee "$LOG"
+
+# Abre o arquivo de log no Notepad
+powershell.exe Start-Process notepad.exe "$LOG"
---
+++ github/sincronizar-github-pc-para-web-e-web-para-pc/sincronizar-web-pc-v6.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+clear
+echo
+echo '# ------------------------------ #'
+echo '#  INICIO DO SINCRONISMO WEB-PC  #'
+echo '# ------------------------------ #'
+echo
+
+echo '# -> cd /d/_GITHUB/templates <- #'
+cd /d/_GITHUB/templates
+echo
+
+echo '# -> pwd <- #'
+pwd
+echo
+
+# Gera nome do log com data e hora
+DATA=$(date +"%Y-%m-%d")
+HORA=$(date +"%Hh%Mm%Ss")
+LOG="D:/_GITHUB/logs/LOG-WEB-PC-$DATA-$HORA.txt"
+
+{
+echo '# -> git status <- #'
+git status
+
+echo '# -> git fetch <- #'
+git fetch
+
+echo '# -> git pull <- #'
+git pull
+
+echo '# -> git log -3 <- #'
+git log -3
+
+
+echo '# --------------------------- #'
+echo '#  FIM DO SINCRONISMO WEB-PC  #'
+echo '# --------------------------- #'
+} | tee "$LOG"
+
+# Abre o arquivo de log no Notepad
+powershell.exe Start-Process notepad++.exe "$LOG"
